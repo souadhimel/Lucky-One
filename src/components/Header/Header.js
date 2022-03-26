f7526982aa791fdfd1c9b2bd0921d2549ee39f8c
Souad Himel <souad.anam@gmail.com>
2022-03-26 10:12:11 +0600

    data created

---
+++ src/components/Header/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Header.css";
+
+const Header = () => {
+  return (
+    <div className="header">
+      <h1> Find your favorite team members </h1>
+      <p>The one and only solution for your dream work</p>
+    </div>
+  );
+};
+
+export default Header;
