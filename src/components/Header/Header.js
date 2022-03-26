d3e48d393788e6d286cb6a46b7ec17c547b5d83d
Souad Himel <souad.anam@gmail.com>
2022-03-26 12:58:24 +0600

    read me added

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,7 +4,7 @@ import "./Header.css";
 const Header = () => {
   return (
     <div className="header">
-      <h1> Find your favorite team members </h1>
+      <h1> Find your favorite web-dev team members </h1>
       <p>
         The one and only solution for your dream work in your door steps!!!!!!!!
       </p>
